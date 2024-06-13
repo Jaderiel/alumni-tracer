1cca0e40ba37a20435e260f95f23931879438232
Jaderiel <jabuela22@gmail.com>
2024-06-13 21:48:25 +0800

    refactor login title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up / Sign In Form</title>
+    <title>Sign In</title>
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
     <!-- <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}"> -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
